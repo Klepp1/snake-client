67bb0c81967b57e89733e098ecaed7dcc80784c0
Kyler <kylerlepp8@gmail.com>
2022-04-18 12:16:35 -0700

    added a successful connection message and send our name data to the server

--- client.js
+++ client.js
@@ -7,6 +7,12 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log('Incoming data!', data);
   });
+  conn.on('connect', () => {
+    console.log('Successfully connected!');
+  });
+  conn.on('connect', () => {
+    conn.write('Name: KYL');
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
